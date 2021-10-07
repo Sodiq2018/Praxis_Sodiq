c6a2fc3ca2349029d4f18a9f4429ba5d11ff139b
Sodiq2018 <sodiq.inf18@student.unu-jogja.ac.id>
2021-10-07 02:37:05 -0700

    edit pertemuan ke 4

--- 01 04/mysite/tugas/views.py
+++ 01 04/mysite/tugas/views.py
@@ -3,9 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from. import models
 
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.POST:
+
+        data = request.POST['nama']
+        #print(data)
+
+        models.tugas.objects.create(nama = request.POST ['nama'])
+
+    hasil1 = models.tugas.objects.all()
+    return render(request, 'index.html', {'isi' : hasil1})
+
+def about(request):
+    return HttpResponse("Halaman About")
     
