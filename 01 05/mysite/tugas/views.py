fcfd0e6dadc229c36dcec93305a3e7c93ee48802
Sodiq2018 <sodiq.inf18@student.unu-jogja.ac.id>
2021-10-08 00:13:10 -0700

    edit lagi

--- 01 05/mysite/tugas/views.py
+++ 01 05/mysite/tugas/views.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,11 +11,15 @@ def index(request):
     if request.POST:
 
         data = request.POST['nama']
-        #print(data)
+        print(data)
 
         models.tugas.objects.create(nama = request.POST ['nama'])
 
     hasil1 = models.tugas.objects.all()
     return render(request, 'index.html', {'isi' : hasil1})
+
+def hapus(request, id):
+    models.tugas.objects.filter(id= id).delete()
+    return redirect('/')
 
     
