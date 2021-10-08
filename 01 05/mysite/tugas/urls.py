b8cd912ed0dbfff5fb57b97a0c8b6303da6d5ca8
Sodiq2018 <sodiq.inf18@student.unu-jogja.ac.id>
2021-10-08 02:12:11 -0700

    edit pertemuan 5

--- 01 05/mysite/tugas/urls.py
+++ 01 05/mysite/tugas/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hapus/<id>', views.hapus)
+    path('hapus/<id>', views.hapus),
+    path('detail/<id>', views.detail),
+    path('edit/<id>' , views.edit),
 ]
--- 01 05/mysite/tugas/views.py
+++ 01 05/mysite/tugas/views.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.shortcuts import redirect, render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from. import models
 
 
@@ -22,4 +22,21 @@ def hapus(request, id):
     models.tugas.objects.filter(id= id).delete()
     return redirect('/')
 
-    
+def detail (request, id):
+    data = models.tugas.objects.filter(id= id).first()
+    print(data)
+    return render(request, "detail.html", {
+        "detailData": data,
+    })
+
+def edit(request, id):
+    if request.POST:
+        input = request.POST['nama']
+        print(input)
+        models.tugas.objects.filter(id= id).update(nama = input)
+        return redirect('/')
+
+    data = models.tugas.objects.filter(id = id).first()
+    return render(request, "edit.html", {
+        "detailData" : data,
+    })    
