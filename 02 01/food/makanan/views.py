b604bec139ff842611ed850f7b18bfb0be1f96f2
Sodiq2018 <sodiq.inf18@student.unu-jogja.ac.id>
2021-10-11 02:11:54 -0700

    aplikasi makanan

---
+++ 02 01/food/makanan/views.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.shortcuts import render
+
+# Create your views here.
+
+def readMakanan(req):
+    if req.POST:
+        input_jenis = req.POST["jenis"]
+        input_nama = req.POST["nama"]
+        input_harga = req.POST["harga"]
+        models.makanan.objects.create(jenis = input_jenis, nama= input_nama, harga = input_harga)
+    data = models.makanan.objects.all()
+    return render(req, "makanan/index.html", {
+        "data": data,
+    })
