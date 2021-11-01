a7b0d06934b8f68dad8c78df54da6e397d9acf5e
Sodiq2018 <sodiq.inf18@student.unu-jogja.ac.id>
2021-10-31 23:49:17 -0700

    cekcek

--- 02 01/food/food/settings.py
+++ 02 01/food/food/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'food.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cek',
+        'USER': 'postgres',
+        'PASSWORD': 'Muhammad13',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
